360c0f1d0a78aeeb45280605eefacb28d9f551aa
Arman Ullah <arman.ullah@outlook.com>
2019-05-02 03:37:07 -0500

    New Page for Gauge List Map Created

---
+++ src/app/gauge-list-map/gauge-list-map.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gauge-list-map',
+  templateUrl: './gauge-list-map.page.html',
+  styleUrls: ['./gauge-list-map.page.scss'],
+})
+export class GaugeListMapPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
